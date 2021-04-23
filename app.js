4b31d52787a39b945826b07042cde6f8abe6761c
Esther Perez Serrajordi <estherperezserrajordi@MacBook-Pro-de-Esther.local>
2021-04-23 17:40:39 +0200

    Solved connection error with db

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ require('./configs/session.config')(app);
 //require('./configs/passport.config')(app);
 
 // Routes middleware goes here
-const index = require('./routes/index.routes');
-const authRoutes = require('./routes/auth.routes');
-const privateRouter = require('./routes/auth.routes');
+const index = require('./routes/index');
+// const authRoutes = require('./routes/auth.routes');
+// const privateRouter = require('./routes/auth.routes');
 app.use('/', index);
-app.use('/auth', authRoutes);
-app.use('/private', privateRouter);
+// app.use('/auth', authRoutes);
+// app.use('/private', privateRouter);
 
 module.exports = app;
--- configs/debbuger.config.js
+++ configs/debbuger.config.js
@@ -1,4 +1,6 @@
-const app_name = require('./package.json').name;
+const path = require('path')
+
+const app_name = require('../package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 module.exports = debug;
